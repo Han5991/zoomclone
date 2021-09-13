fcf8b68b67361a56ea3112e5eba32dca60c743a2
rewq985 <bnbt3@naver.com>
2021-09-13 21:47:40 +0900

    프론트랑 백엔드 메시지 주고 받기 성공

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,17 @@
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => {
+    console.log("Connected to Server ✅");
+});
+
+socket.addEventListener("message", (message) => {
+    console.log("New message: ", message.data);
+});
+
+socket.addEventListener("close", () => {
+    console.log("Disconnected from Server ❌");
+});
+
+setTimeout(() => {
+    socket.send("프론트에서 보낸 메시지입니다.");
+}, 2000);
