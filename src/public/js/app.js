e3097b851babaf6b1f784de83fa9699acd7617c3
rewq985 <bnbt3@naver.com>
2021-09-15 19:07:16 +0900

    여러브라우저 접속과 메시지 전부 보내기

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,3 +1,5 @@
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
 const socket = new WebSocket(`ws://${window.location.host}`);
 
 function handleOpen() {
@@ -14,6 +16,11 @@ socket.addEventListener("close", () => {
     console.log("Disconnected from Server ❌");
 });
 
-setTimeout(() => {
-    socket.send("프론트에서 보낸 메시지입니다.");
-}, 2000);
+function handleSubmit(event) {
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(input.value);
+    input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit);
