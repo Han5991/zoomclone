5d687b96f324441ae88afe0c6307c6434c2ccc46
rewq985 <bnbt3@naver.com>
2021-09-16 19:22:13 +0900

    닉네임과 메시지 구분인대 뭐 굳이... 닉네임을 세션에 저장하지 않는건 뭐지? 너무 간단하게 프로젝트를 하려는거 같다.

--- src/server.js
+++ src/server.js
@@ -11,23 +11,18 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
-const handleListen = () => console.log('Listening on http://localhost:3000');
 const server = http.createServer(app);
 const wss = new WebSocket.Server({server});
-
-function onSocketClose() {
-    console.log("Disconnected from the Browser ❌");
-}
 
 const sockets = [];
 
 wss.on("connection", (socket) => {
     sockets.push(socket);
     console.log("Connected to Browser ✅");
-    socket.on("close", onSocketClose);
+    socket.on("close", () => console.log("Disconnected from the Browser ❌"));
     socket.on("message", (message) => {
-        sockets.forEach((aSocket) => aSocket.send(message.toString()));
+        sockets.forEach(aSocket => aSocket.send(message.toString()));
     });
 });
 
-server.listen(3000, handleListen);
+server.listen(3000, () => console.log('Listening on http://localhost:3000'));
