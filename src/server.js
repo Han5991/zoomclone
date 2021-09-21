a3faf0f9b5fb0c10d48c668967dd3de8e6ac61ce
rewq985 <bnbt3@naver.com>
2021-09-21 14:11:38 +0900

    Socket.io 기능 몇 가지 살펴보기 - 백엔드에서 프론트의 펑션을 실행 시킬 수 있다. 개쩌네

--- src/server.js
+++ src/server.js
@@ -14,10 +14,9 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket) => {
-    socket.on("enter_room", (msg, done) => {
-        console.log(msg);
+    socket.on("enter_room", (roomName, done) => {
         setTimeout(() => {
-            done();
+            done(roomName);
         }, 1000);
     });
 });
